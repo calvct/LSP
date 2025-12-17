2a00b7b7e8b6f56caaf7f3be5efae01c00d44fd9
Calvin Christian Tjong <calvinchristiant3@gmail.com>
2025-12-17 20:00:47 +0700

    Initial Commit

---
+++ LSP/LSPApp.swift
@@ -0,0 +1,17 @@
+//
+//  LSPApp.swift
+//  LSP
+//
+//  Created by Calvin Christian Tjong on 17/12/25.
+//
+
+import SwiftUI
+
+@main
+struct LSPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
